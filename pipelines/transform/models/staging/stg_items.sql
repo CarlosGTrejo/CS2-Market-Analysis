d13a7dc69636e757f5eac968a7548e0d0098aeaa
Carlos Guadarrama-Trejo <37058445+CarlosGTrejo@users.noreply.github.com>
2026-03-31 23:45:38 -0500

    fixed casting, and added string datatype comment for bucket group id

--- pipelines/transform/models/staging/stg_items.sql
+++ pipelines/transform/models/staging/stg_items.sql
@@ -6,8 +6,8 @@ renamed_and_casted AS (
     SELECT
         -- Identifiers
         CAST(asset_description__market_hash_name AS STRING) AS item_name,
-        CAST(asset_description__classid AS INT64) AS class_id,
-        CAST(asset_description__market_bucket_group_id AS INT64) AS market_bucket_group_id,
+        SAFE_CAST(asset_description__classid AS INT64) AS class_id,
+        CAST(asset_description__market_bucket_group_id AS STRING) AS market_bucket_group_id,
         
         -- Characteristics
         CAST(asset_description__type AS STRING) AS item_type,
@@ -19,14 +19,14 @@ renamed_and_casted AS (
         CAST(asset_description__commodity AS BOOL) AS is_commodity,
         
         -- Pricing & Metrics
-        CAST(sell_listings AS INT64) AS sell_listings_count,
-        CAST(sell_price AS INT64) AS sell_price_cents,
-        CAST(sell_price AS NUMERIC) / 100 AS sell_price_usd,
+        SAFE_CAST(sell_listings AS INT64) AS sell_listings_count,
+        SAFE_CAST(sell_price AS INT64) AS sell_price_cents,
+        SAFE_CAST(sell_price AS NUMERIC) / 100 AS sell_price_usd,
         -- Extract numbers/decimals from text and cast safely:
         SAFE_CAST(REGEXP_REPLACE(sale_price_text, r'[^0-9\.]', '') AS NUMERIC) AS sale_price_usd_from_text,
         
         -- Dates
-        CAST(snapshot_date AS DATE) AS snapshot_date,
+        SAFE_CAST(snapshot_date AS DATE) AS snapshot_date,
         
         -- dlt pipeline metadata
         CAST(_dlt_id AS STRING) AS dlt_id,
